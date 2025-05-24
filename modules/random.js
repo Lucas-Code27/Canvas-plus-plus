b82616e23913e087aca473657a71575b4a8759a9
Lucas-Code27 <83315414+Lucas-Code27@users.noreply.github.com>
2025-05-24 11:55:03 -0700

    Random module complete

--- modules/random.js
+++ modules/random.js
@@ -1,3 +1,7 @@
-export function randi(max) {
-  return Math.floor(Math.random() * max + 1);
+export function randi(min, max) {
+  return Math.floor(Math.random() * max - min + 1) + min
+}
+
+export function randf(min, max){
+  return Math.random() * (max - min + 1) + min
 }
